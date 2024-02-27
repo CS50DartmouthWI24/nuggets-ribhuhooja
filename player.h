71b3caaa4d499fabf967412622751544a01b55cb
tayebmohammadi <tayeb.mohammadi.26@dartmouth.edu>
2024-02-27 12:38:52 -0500

    Updated player.h with inactive functionally to keep track of inactive players

--- player.h
+++ player.h
@@ -5,13 +5,15 @@
  * The struct keeps track of all information that defines a player 
  *
  * Paul Cherian, COSC 50, Febuary 2024
+ * Ribhu Hooja, February 2024 - made opaque, protected from multiple includes,
+ * added functionality needed by grid
  * 
  */
 
+#ifndef __PLAYER_H
+#define __PLAYER_H
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdbool.h>
 #include "message.h"
 #include "grid.h"
@@ -22,44 +24,44 @@ typedef struct player {
   int y;
   grid_t* visibleGrid;
   int gold;
+  bool isActive;
   char* name;
   char* letter; 
-  addr_t address; // this should be the address not the pointer to address----------------------------
+  addr_t* address;
 } player_t;
 
 
 /************* player_new *************/
-/* Creates a new player struct
+/** Creates a new player struct
  * Caller provides: 
- *  (x,y) coordaintes, name and address of client who is using the player
+ *  @param x coordinates,
+ *  @param y coordinates,
+ *  @param name,
+ *  @param address of client who is using the player
  * We do: 
  *  Initialize a new player_t* data struct with the parameters
  * We return:
  *  The initialized player, NULL if any failure
- * Notes: the caller should free memory for name and the player structure 
- *  when done.
  *
 */
-player_t* player_new (grid_t* playerGrid, addr_t address, int x, int y, char* name, char letter );
+player_t* player_new(player_t*, int x, int y, char* name, addr_t* address);
 
 
 /************* player_delete *************/
-/* Deletes a player struct
+/** Deletes a player struct
  * Caller provides: 
- *  the player to delete
+ *  @param player to delete
  * We do: 
  *  Delete the player, freeing all associated structs and their memory
- * We return:
- *  void
  *
 */
 void player_delete(player_t* player);
 
 /************* player_getX *************/
-/* 
+/**
  * Get the x-coordinate of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the x-coordinate.
+ *  @param pointer to the player from which to get the x-coordinate.
  * We do: 
  *  Retrieve the x-coordinate of the player.
  * We return: 
@@ -68,10 +70,10 @@ void player_delete(player_t* player);
 int player_getX(const player_t* player);
 
 /************* player_getY *************/
-/* 
+/**
  * Get the y-coordinate of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the y-coordinate.
+ * @param pointer to the player from which to get the y-coordinate.
  * We do: 
  *  Retrieve the y-coordinate of the player.
  * We return: 
@@ -80,10 +82,10 @@ int player_getX(const player_t* player);
 int player_getY(const player_t* player);
 
 /************* player_getVisibleGrid *************/
-/* 
+/**
  * Get the visible grid for the player
  * Caller provides: 
- *  A pointer to the player from whom to get the visible grid.
+ * @param pointer to the player from which to get the visible grid.
  * We do: 
  *  Retrieve the pointer to the visible grid of the  player.
  * We return: 
@@ -92,10 +94,10 @@ int player_getY(const player_t* player);
 grid_t* player_getVisibleGrid(const player_t* player);
 
 /************* player_getGold *************/
-/* 
+/** 
  * Get the gold amount of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the gold amount.
+ * @param pointer to the player from which to get the gold amount.
  * We do: 
  *  Retrieve the gold amount of the player.
  * We return: 
@@ -104,10 +106,10 @@ grid_t* player_getVisibleGrid(const player_t* player);
 int player_getGold(const player_t* player);
 
 /************* player_getName *************/
-/* 
+/** 
  * Get the name of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the name.
+ * @param pointer to the player from which to get the name.
  * We do: 
  *  Retrieve the name of the player.
  * We return: 
@@ -116,10 +118,10 @@ int player_getGold(const player_t* player);
 char* player_getName(const player_t* player);
 
 /************* player_getLetter *************/
-/* 
+/** 
  * Get the letter of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the letter.
+ * @param pointer to the player from which to get the letter.
  * We do: 
  *  Retrieve the letter of the player.
  * We return: 
@@ -128,10 +130,10 @@ char* player_getName(const player_t* player);
 char player_getLetter(const player_t* player);
 
 /************* player_getAddress *************/
-/* 
+/** 
  * Get the address of the player
  * Caller provides: 
- *  A pointer to the player from whom to get the address.
+ * @param pointer to the player from whom to get the address.
  * We do: 
  *  Retrieve the address of the  player.
  * We return: 
@@ -140,79 +142,56 @@ char player_getLetter(const player_t* player);
 addr_t player_getAddress(const player_t* player);
 
 /************* player_setX *************/
-/* 
+/** 
  * Set the x-coordinate of the player
  * Caller provides: 
- *  A pointer to the player and the new x-coordinate.
+ * @param pointer to the player,
+ * @param new x-coordinate.
  * We do: 
  *  Set the x-coordinate of the player to the new value.
- * We return: 
- *  none
  */
 void player_setX(player_t* player, int x);
 
 /************* player_setY *************/
-/* 
+/** 
  * Set the y-coordinate of the player
  * Caller provides: 
- *  A pointer to the player and the new y-coordinate.
+ * @param pointer to the player
+ * @param new y-coordinate.
  * We do: 
  *  Set the y-coordinate of the player to the new value.
- * We return: 
- *  none
  */
 void player_setY(player_t* player, int y);
 
-
-/************* player_move *************/
-/* 
- * Set the x and y coordinates of the player
- * Caller provides: 
- *  A pointer to the player and new  x and y coordinates.
- * We do: 
- *  Move the player diagonnaly x and y position
- * We return: 
- *  none
- */
-
-
-void player_move(player_t* player, int x, int y);
-
-
-/************* player_setGold *************/
-/* 
- * Set the gold amount for the player
- * Caller provides: 
- *  A pointer to the player and the gold amount.
- * We do: 
- *  Set the gold amount of the player to the new amount.
- * We return: 
- *  none
- */
-void player_setGold(player_t* player, int gold);
-
 /************* player_setVisibleGrid *************/
-/* 
+/** 
  * Set the visible grid for the player
  * Caller provides: 
- *  A pointer to the player and a pointer to the new grid.
+ * @param pointer to the player
+ * @param pointer to the new grid.
  * We do: 
  *  Set the visible grid of the player to the grid.
- * We return: 
- *  none
  */
 void player_setVisibleGrid(player_t* player, grid_t* visibleGrid);
 
+/************* player_setGold *************/
+/**
+ * Set the gold amount for the player
+ * Caller provides: 
+ * @param pointer to the player
+ * @param gold amount.
+ * We do: 
+ *  Set the gold amount of the player to the new amount.
+ */
+void player_setGold(player_t* player, int gold);
 
 /************* player_sendMessage *************/
-/* 
+/** 
  * Send message to a player
  * Caller provides: 
- *  A pointer to the player and a pointer to message string
+ * @param pointer to the player
+ * @param pointer to message string
  * We do: 
  *  Send the message to player
- * We return: 
- *  none
  */
-
 void player_sendMessage(player_t* player, char* message);

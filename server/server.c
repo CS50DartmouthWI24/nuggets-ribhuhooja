7a74c7bd9979578f9d2a647520a3553e5797d06c
Sam Starrs <sam.starrs24@gmail.com>
2024-03-01 14:22:07 -0500

    Updated with new Makefile and WIP server.c

--- server/server.c
+++ server/server.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "player.h"
 // #include "file.h"
 #include "message.h"
+// #include "grid.h"
 
 static void parseArgs(const int argc, char* argv[],
                       char** map, char** seed);
@@ -21,7 +23,6 @@ const int GOLDTOTAL = 250;      // amount of gold in the game
 const int GoldMinNumPiles = 10; // minimum number of gold piles
 const int GoldMaxNumPiles = 30; // maximum number of gold piles
 const int TIMEOUT = 15;
-const char letters[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 const int ncols;
 const int nrows;
@@ -111,9 +112,9 @@ static void countMapDimensions(FILE* fp, int* ncolsp, int* nrowsp) {
     // }
 }
 
-static bool handleMessage(void* arg, const addr_t from, const char* buf) {
+static bool handleMessage(void* arg, const addr_t from, const char* message) {
 
-    char* message = buf;
+    // char* message = buf;
 
     if (strncmp(message, "PLAY ", strlen("PLAY ")) == 0) { // PLAY
         const char* content = message + strlen("PLAY ");
@@ -133,11 +134,16 @@ static bool handleMessage(void* arg, const addr_t from, const char* buf) {
 static void handlePlay(void* arg, const addr_t from, const char* content) {
 
     // SYNTAX: PLAY real name
-
-    printf("Player name: %s\n", content);
-
+    const addr_t* to = &from;
+    char* name = content;
+    printf("Player name: %s\n", name);
+    player_t* player = player_new(to, 0, 0, name, 'A');
+    printf("%s\n", player_getName(player));
+    char* display = "DISPLAY\n123456789\n123456789\n123456789\n";
+    message_send(from, display);
+    
     // if(content != NULL) {
-    //     //if playerCount != max
+    //    // if playerCount != max
     //     int x = randomNumber(0, ncols);
     //     int y = randomNumber(0, nrows);
     //     player_t* player = player_new(x, y, content, from);
@@ -157,42 +163,57 @@ static void handlePlay(void* arg, const addr_t from, const char* content) {
 
 static void handleKey(void* arg, const addr_t from, const char* content) {
 
-    // // SYNTAX: KEY k
+    // SYNTAX: KEY k
 
-    // if(content != NULL) {
-    //     char letter = content;
-    //     switch (letter) {
-    //         case 'Q': keyQ(); break;
-    //         case 'B': ... code for letter=='B'; break;
-    //         case 'C': ... code for letter=='C'; break;
-    //         default:  ... code for letter not matching any case above.
-    //     }
-    // }
+    if(content != NULL) {
+        char letter = content;
+        switch (letter) {
+            case 'Q': keyQ(); break; // quit
+            case 'q': keyQ(); break; // quit
+            case 'h': game_move(game, from, -1, 0); break; // left
+            case 'l': game_move(game, from, 1, 0); break; // right
+            case 'j': game_move(game, from, 0, 1); break; // down
+            case 'k': game_move(game, from, 0, -1); break; // up
+            case 'y': game_move(game, from, -1, -1); break; // up and left
+            case 'u': game_move(game, from, 1, -1); break; // up and right
+            case 'b': game_move(game, from, -1, 1); break; // down and left
+            case 'n': game_move(game, from, 1, 1); break; // down and right
+            case 'H': game_longMove(game, from, -1, 0); break; // LONG left
+            case 'L': game_longMove(game, from, 1, 0); break; // LONG right
+            case 'J': game_longMove(game, from, 0, 1); break; // LONG down
+            case 'K': game_longMove(game, from, 0, -1); break; // LONG up
+            case 'Y': game_longMove(game, from, -1, -1); break; // LONG up and left
+            case 'U': game_longMove(game, from, 1, -1); break; // LONG up and right
+            case 'B': game_longMove(game, from, -1, 1); break; // LONG down and left
+            case 'N': game_longMove(game, from, 1, 1); break; // LONG down and right
+            default: message_send(from, "ERROR - key not recognized.")
+        }
+    }
 
 }
 
 static void handleSpectate(void* arg, const addr_t from, const char* content) {
 
-    // // Add spectator to game
-    // game_addspectator(from, content);
+    // Add spectator to game
+    game_addspectator(from, content);
 
-    // // Send grid message
-    // char* gridMessage = malloc((sizeof(char) * strlen("GRID 1 1")) + 1)
-    // sprintf(gridMessage, "GRID %d %d", nrows, ncols);
-    // message_send(from, gridMessage);
-    // // Send gold message
-    // // need function to find player from address int n = player_getGold()
-    // char* goldMessage = malloc((sizeof(char) * strlen("GOLD 1 1 1")) + 1)
-    // sprintf(goldMessage, "GOLD %d %d %d", n, p, r);
-    // message_send(from, goldMessage);
-    // // Send full map
-    // char* displayMessage = malloc((sizeof(char) * (strlen("DISPLAY\n") + strlen(grid_getDisplay(game.grid)))) + 1)
-    // char* string = grid_getDisplay(game.grid);
-    // sprintf(displayMessage, "DISPLAY\n%s", string);
-    // message_send(from, displayMessage);
+    // Send grid message
+    char* gridMessage = malloc((sizeof(char) * strlen("GRID 1 1")) + 1)
+    sprintf(gridMessage, "GRID %d %d", nrows, ncols);
+    message_send(from, gridMessage);
+    // Send gold message
+    // need function to find player from address int n = player_getGold()
+    char* goldMessage = malloc((sizeof(char) * strlen("GOLD 1 1 1")) + 1)
+    sprintf(goldMessage, "GOLD %d %d %d", n, p, r);
+    message_send(from, goldMessage);
+    // Send full map
+    char* displayMessage = malloc((sizeof(char) * (strlen("DISPLAY\n") + strlen(grid_getDisplay(game.grid)))) + 1)
+    char* string = grid_getDisplay(game.grid);
+    sprintf(displayMessage, "DISPLAY\n%s", string);
+    message_send(from, displayMessage);
 
 }
 
-static void keyQ(void* arg, const addr_t from, const char* content) {
+static void keyQ(void* arg, const addr_t from, const char* content) { // QUIT
 
 }

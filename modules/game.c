e675fab5e2b868fc20e522265dd09e1ad0263d40
tayebmohammadi <tayeb.mohammadi.26@dartmouth.edu>
2024-03-02 00:14:41 -0500

    game with all the functions needed

--- modules/game.c
+++ modules/game.c
@@ -314,7 +314,7 @@ static void sendGoldMessage(game_t* game, player_t* player, const int goldCollec
 
 
 // to update the visible grid of each player 
-void static game_updateAllVisibleGrids(game_t* game){
+static void game_updateAllVisibleGrids(game_t* game){
     for (int i = 0; i < game->numPlayer; ++i){
         player_t* curr = game->players[i];
         if (!player_isActive(curr)){
@@ -323,6 +323,32 @@ void static game_updateAllVisibleGrids(game_t* game){
         player_setVisibleGrid(curr, grid_generateVisibleGrid(game->masterGrid, player_getVisibleGrid(curr), player_getX(curr), player_getY(curr)));
     }
 
+}
+
+static void displayAllPlayers(game_t* game){
+    if (game == NULL){
+        return;
+    }
+
+    player_t** players = game->players;
+    char* display = "DISPLAY\n";
+    int displayLen = strlen(display);
+    for (int i = 0; i < game->numPlayer; ++i){
+        player_t* player = players[i];
+        if (!player_isActive(player)){
+            continue;
+        }
+        
+        char* gridString = grid_getDisplay(player_getVisibleGrid(player));
+        int gridLen = strlen(gridString);
+        int length = displayLen + gridLen;
+
+        char* message = mem_calloc_assert(length + 1, sizeof(char), "Could not allocate memory for display grid of each player.\n");
+        snprintf(message, length, "DISPLAY\n%s", gridString);
+        player_sendMessage(player, message);
+
+        free(message);
+    }
 }
 
 // A helper funciton that returns the result string
@@ -334,7 +360,7 @@ static char* get_result(game_t* game){
 
     // create a string result aggregate, mallocing enough space to add all the individual results
     int length = (MaxNameLength + 20) * MaxPlayers;
-    char* gmaeOverMessage = mem_malloc_assert(length, "Could not allocate memory for game over message.\n");
+    char* gameOverMessage = mem_malloc_assert(length, "Could not allocate memory for game over message.\n");
 
     for(int i = 0; i < game->numPlayer; ++i){
         player_t* player = game->players[i];
@@ -342,9 +368,10 @@ static char* get_result(game_t* game){
 
         snprintf(result, MaxNameLength + 20, "QUIT GAME OVER:\n");
         snprintf(result, "%c %10d %s",player_getletter(player), player_getGold(player), player_getName(player));
-        strncat(gmaeOverMessage, result,length);
+        strncat(gameOverMessage, result,length);
         // strncat result to resultAggregate
     }
+    return gameOverMessage;
 
     // either in this function, but preferably in another, send the result string to each player
 }
@@ -364,6 +391,8 @@ void game_over(game_t* game){
         player_sendMessage(game->players[i], result);
         player_delete(game->players[i]);
     }
+
+    free(result);
 
 
     // and then free the array of players too
